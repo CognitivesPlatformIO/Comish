6e59e90ba3b77896e29778e1a2a217a90ac69e9b
Azam <insphere.azam@gmail.com>
2017-03-07 12:58:09 +0530

    User profile page

--- static/dev/js/user-profile.js
+++ static/dev/js/user-profile.js
@@ -1,23 +1,23 @@
 (function ($) {
     
 
-    //$('.followBlog').followBlog({
-    //    onSuccess: function (data, obj) {
-    //        var status = $(obj).data('status');
-    //        if($(obj).hasClass('hasStar')) {
-    //            (status == 'unfollow') ? $(obj).addClass('selected') : $(obj).removeClass('selected');
-    //        }
-    //    },
-     //   beforeSend: function (obj) {
-    //        $(obj).find('.fa').addClass('fa-spin fa-spinner');
-    //    },
-    //    onError: function (obj, errorMessage) {
-    //        $().General_ShowErrorMessage({message: errorMessage});
-    //    },
-    //    onComplete: function (obj) {
-    //        $(obj).find('.fa').removeClass('fa-spin fa-spinner');
-    //    }
-    //});
+    $('.FollowUnfollowBlog').followBlog({
+        onSuccess: function (data, obj) {
+            var status = $(obj).data('status');
+            $(obj).get(0).lastChild.nodeValue = " " + status.substr(0,1).toUpperCase()+status.substr(1);
+            var message = ($(obj).data('status') === 'follow') ? 'Blog unfollowed successfully' : 'Blog followed successfully';
+            $.fn.General_ShowNotification({message: message});
+        },
+        beforeSend: function (obj) {
+            //$(obj).find('.fa').addClass('fa-spin fa-spinner');
+        },
+        onError: function (obj, errorMessage) {
+            $().General_ShowNotification({message: errorMessage, type: 'error', timeout: 4000});
+        },
+        onComplete: function (obj) {
+            $(obj).find('.fa').removeClass('fa-spin fa-spinner');
+        }
+    });
     
     //
     // // profile user and writers
